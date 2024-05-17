1398e0cad843173a93323adb8518b8dfc264e3c9
TrollGuyFromMars <laiklialuke@gmail.com>
2024-05-17 04:00:18 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 // Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAuzePkzW-zV39_72PDmniyvHhc99iAwf4",
+  authDomain: "database-project12345.firebaseapp.com",
+  databaseURL: "https://database-project12345-default-rtdb.firebaseio.com",
+  projectId: "database-project12345",
+  storageBucket: "database-project12345.appspot.com",
+  messagingSenderId: "941802942011",
+  appId: "1:941802942011:web:ab71f5e69251b0b0a7761e",
+  measurementId: "G-RS454MJFNQ"
 };
 
 // Initialize Firebase
